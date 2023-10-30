cf9ed77bef6f17b22133065edd6ba2b63324ecce
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-10-30 16:50:29 -0700

    get started with fabric

--- train.py
+++ train.py
@@ -1,9 +1,21 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from lightning.fabric import Fabric
+
+
+def main(cfg: DictConfig) -> None:
+    fabric_args = cfg.fabric if cfg.fabric else {}
+    fabric = Fabric(**fabric_args)
+    fabric.launch()
+
+    device = fabric.device
+    print("device: ", device)
+
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
-def my_app(cfg : DictConfig) -> None:
+def training(cfg : DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
+    main(cfg)
 
 if __name__ == "__main__":
-    my_app()
+    training()
