5a9d8bd90daaf48a5ec0f0885d93adc62836a57b
xju <xju@cern.ch>
2023-11-15 18:28:04 -0800

    add wrapper fn

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,13 @@
-from typing import List
+from typing import List, Callable
+import time
+from pathlib import Path
+from importlib.util import find_spec
 
-from lightning.fabric.loggers import Logger
+
 import hydra
 from omegaconf import DictConfig
+from lightning.fabric.loggers import Logger
+from lightning.pytorch.utilities import rank_zero
 
 # local imports
 from src.utils.pylogger import get_pylogger
@@ -26,3 +31,56 @@ def instantiate_loggers(logger_cfg: DictConfig) -> List[Logger]:
             logger.append(hydra.utils.instantiate(lg_conf))
 
     return logger
+
+def task_wrapper(task_func: Callable) -> Callable:
+    """Optional decorator that wraps the task function in extra utilities.
+
+    Makes multirun more resistant to failure.
+
+    Utilities:
+    - Calling the `utils.close_loggers()` after the task is finished
+    - Logging the exception if occurs
+    - Logging the task total execution time
+    - Logging the output dir
+    """
+
+    def wrap(cfg: DictConfig):
+        """Wrapper function."""
+
+        # execute the task
+        try:
+            start_time = time.time()
+            task_func(cfg=cfg)
+        except Exception as ex:
+            log.exception("")  # save exception to `.log` file
+            raise ex
+        finally:
+            path = Path(cfg.paths.output_dir, "exec_time.log")
+            content = (
+                f"'{cfg.task_name}' execution time: {time.time() - start_time} (s)"
+            )
+            save_file(
+                path, content
+            )  # save task execution time (even if exception occurs)
+            close_loggers()  # close loggers (even if exception occurs so multirun won't fail)
+
+        log.info(f"Output dir: {cfg.paths.output_dir}")
+
+    return wrap
+
+@rank_zero
+def save_file(path: Path, content: str):
+    with open(path, "w") as f:
+        f.write(content)
+
+@rank_zero
+def close_loggers():
+    """Makes sure all loggers closed properly (prevents logging failure during multirun)."""
+
+    log.info("Closing loggers...")
+    for lg in Logger.loggers:
+        lg.close()
+
+    if find_spec("wandb"):
+        import wandb
+        wandb.finish()
