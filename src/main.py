e62e4ee54eec461ac5e5e529f1607e0f6c872398
xju <xju@cern.ch>
2023-11-15 18:36:52 -0800

    main fn

---
+++ src/main.py
@@ -0,0 +1,83 @@
+import pyrootutils
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+OmegaConf.register_new_resolver("eval", eval)
+OmegaConf.register_new_resolver("sum", lambda x, y: x + y)
+OmegaConf.register_new_resolver("gen_list", lambda x, y: [x] * y)
+
+from typing import List, Tuple
+
+from lightning.pytorch.core import LightningDataModule, LightningModule
+from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.loggers.logger import Logger
+from lightning.pytorch.callbacks import Callback
+
+
+from src import utils
+
+log = utils.get_pylogger(__name__)
+
+
+@utils.task_wrapper
+def main(cfg: DictConfig) -> Tuple[dict, dict]:
+    """Trains or Evaluation the model.
+
+    This method is wrapped in optional @task_wrapper decorator which applies extra utilities
+    before and after the call.
+
+    Args:
+        cfg (DictConfig): Configuration composed by Hydra.
+    """
+
+    # set seed for random number generators in pytorch, numpy and python.random
+    if cfg.get("seed"):
+        seed_everything(cfg.seed, workers=True)
+
+    stage = cfg.get("stage", "fit")
+
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
+    datamodule.setup(stage)
+
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(cfg.model)
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+
+    if stage == "fit":
+
+        log.info("Starting training!")
+        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+
+    elif stage == "test":
+        log.info("Starting testing!")
+        ckpt_path = trainer.checkpoint_callback.best_model_path
+        if ckpt_path == "":
+            log.warning("Best ckpt not found! Using current weights for testing...")
+            ckpt_path = None
+        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+        log.info(f"Best ckpt path: {ckpt_path}")
+    else:
+        raise ValueError(f"Unknown stage: {stage}")
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="main.yaml")
+def lightning_main(cfg : DictConfig) -> None:
+    main(cfg)
+
+if __name__ == "__main__":
+    lightning_main()
